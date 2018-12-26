7ea7581d5fa6fa064ab7c59d55a0f4b3926e2079
52hezi <18035762253@163.com>
2018-12-26 15:23:34 +0800

    第一次上传项目,先关联库和文件夹,然后add,然后上传到本地服务器,最后推到gitub的公共空间

---
+++ script/test_01.py
@@ -0,0 +1,10 @@
+import allure
+class Test_a:
+    def test_a(self):
+        print("\n1111")
+        assert 1
+
+    def test_b(self):
+        print("\n2222")
+        print("\n3333")
+        assert 0
